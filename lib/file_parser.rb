2261696759ac6232b875443705a3acb61f0ea8d0
Cara Bell <carabell1994@aim.com>
2020-11-02 10:19:52 +0000

    initial pseudocode

---
+++ lib/file_parser.rb
@@ -0,0 +1,14 @@
+# TO DO:
+# Build initialize method taking filepath as an argument
+# Build a file_parser method using File.foreach
+# Build a visits_per_page method using group_by
+# Build a page_visit_counter method using map and count
+# Build a page_visit_sorter method using sort_by and reverse
+# Build a display_page_visits method using .join ?? Or combine with above
+# Build a unique_visits method using .uniq.count and .sort_by
+# Build a display_unique_visits method using .join ?? Or combine with above
+
+class FileParser
+  def initialize
+  end
+end
