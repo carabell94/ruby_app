1fa595bd5ef269939ec94b510aa19334ddf52ef7
Cara Bell <carabell1994@aim.com>
2020-11-02 20:35:17 +0000

    built visit_spec

---
+++ spec/visit_spec.rb
@@ -0,0 +1,17 @@
+describe Visit do
+  describe 'initialize' do
+    it 'should return a page url' do
+      page_url = '/help_page/1'
+      ip_address = '126.318.035.038'
+      visit = Visit.new(page_url, ip_address)
+      expect (visit.page_url).to eq('/help_page/1')
+    end
+
+    it 'should return an ip address' do
+      page_url = '/help_page/1'
+      ip_address = '126.318.035.038'
+      visit = Visit.new(page_url, ip_address)
+      expect (visit.ip_address).to eq('126.318.035.038')
+    end
+  end
+end
